4bfe33c8a6eb592b3e006b86cafed9e13c996dfc
white <white.freddie95@gmail.com>
2021-02-11 15:58:46 -0800

    initial commit frederick

--- application/frontend/src/pages/Frederick.js
+++ application/frontend/src/pages/Frederick.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import '../assets/bootstrap/css/bootstrap.min.css';
+import '../assets/css/rinay.css';
 
 const Frederick = () => {
   return (
-    <div>
-      <h1>Frederick</h1>
+      <div>
+      <Navbar bg="dark" variant="dark" sticky="top">
+        <Navbar.Brand href="#home">Frederick</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="#About Me">About Me</Nav.Link>
+          <Nav.Link href="#Portfolio">Portfolio</Nav.Link>
+          <Nav.Link href="#Contact">Contact</Nav.Link>
+        </Nav>
+      </Navbar>
     </div>
+    
   );
 };
 

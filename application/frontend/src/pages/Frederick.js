42b31698a751dc8de18fd543fe4550c5d55a93c4
white <white.freddie95@gmail.com>
2021-02-15 10:27:42 -0800

    added cover page. Need to fill out about me still

--- application/frontend/src/pages/Frederick.js
+++ application/frontend/src/pages/Frederick.js
@@ -1,23 +1,59 @@
 import React from 'react';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
 import '../assets/bootstrap/css/bootstrap.min.css';
-import '../assets/css/rinay.css';
+import '../assets/css/frederick.css';
+import Navbar from "react-bootstrap/Navbar"
+import Nav from 'react-bootstrap/Nav'
+import Container from 'react-bootstrap/Container'
+//import Row from 'react-bootstrap/Row'
+//import Col from 'react-bootstrap/Col'
+import Card from 'react-bootstrap/Card'
+
 
 const Frederick = () => {
   return (
-      <div>
-      <Navbar bg="dark" variant="dark" sticky="top">
-        <Navbar.Brand href="#home">Frederick</Navbar.Brand>
-        <Nav className="mr-auto">
-          <Nav.Link href="#About Me">About Me</Nav.Link>
-          <Nav.Link href="#Portfolio">Portfolio</Nav.Link>
-          <Nav.Link href="#Contact">Contact</Nav.Link>
+    <body className="bg">
+    
+      <header id="header">
+      <Navbar bg="dark" variant="dark" stick="top">
+        <Navbar.Brand id="logo">Frederick</Navbar.Brand>
+        <Nav className="ml-auto">
+          <Nav.Link href="#aboutMe">About Me</Nav.Link>
+          <Nav.Link href="#portfolio">Portfolio</Nav.Link>
+          <Nav.Link href="#contact">Contact</Nav.Link>
         </Nav>
       </Navbar>
+    </header>
+     
+
+    <section id="hero">
+    <div className="hero-container">
+        <h1>frederick.portfolio<span class="blinking"> .</span></h1>
+        <h2>Software Engineer</h2>
+        <a href="#aboutMe" class="btn-get-started">About Me</a>
     </div>
-    
+  </section>
+
+  <Container>
+    <div id="wrapper">
+      <div className="w-100 h-100 p-3">
+        <h2>Testing</h2>
+      </div>
+    </div>
+  </Container>
+  
+  </body>
   );
 };
 
 export default Frederick;
+
+/* <Card className="bg-dark text-white" style={{width: '50%' }} >
+<Card.Img variant="top" rounded/>
+<Card.Body>
+  <Card.Text>
+  I am a computer science student in my senior year at SFSU. I
+      enjoy creating mobile apps and learning new technologies. In my free 
+      time I like to program, rock climb, read, and listen to music.
+  </Card.Text>
+</Card.Body>
+</Card>  */

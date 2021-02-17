4e41d76e3a170b84b718df6ffed5c63c20aa254a
white <white.freddie95@gmail.com>
2021-02-17 15:45:35 -0800

    added about me section. Has work experience and skills

--- application/frontend/src/pages/Frederick.js
+++ application/frontend/src/pages/Frederick.js
@@ -7,10 +7,16 @@ import Container from 'react-bootstrap/Container'
 //import Row from 'react-bootstrap/Row'
 //import Col from 'react-bootstrap/Col'
 import Card from 'react-bootstrap/Card'
+//import CardGroup from 'react-bootstrap/CardGroup'
+import CardDeck from 'react-bootstrap/CardDeck'
 
+
+//import image here
 
 const Frederick = () => {
   return (
+    <div>
+      
     <body className="bg">
     
       <header id="header">
@@ -33,15 +39,60 @@ const Frederick = () => {
     </div>
   </section>
 
-  <Container>
-    <div id="wrapper">
-      <div className="w-100 h-100 p-3">
-        <h2>Testing</h2>
-      </div>
-    </div>
+<section id="work">
+  <Container fluid>
+        <CardDeck>
+          <Card bg="dark" text="white">
+            <Card.Header as="h3">Work Experience</Card.Header>
+            <Card.Body>
+              <Card.Text>
+                <h5>SOFTWARE ENGINEERING INTERN, ROOSTOO LABS LIMITED</h5>
+                <ul>
+                  <li>Worked as a software engineering intern on a mobile app that focused on mock cryptocurrency trading</li>
+                  <li>Worked on the existing app that uses the Flutter framework</li>
+                  <li>Helped to design and implement various new features for the app</li>
+                  <li>Utilized Flutter, API’s, MVVM design, Android, and IOS</li>
+                </ul>
+                <h5>INTERN, GOLDEN GATE ELECTRIC VEHICLE ASSOCIATION</h5>
+                <ul>
+                  <li>Collected information on EV parking by utilizing GIS and open data</li>
+                  <li>Implemented a Java front end with SQL database to collect and sort data</li>
+                  <li>Utilized Excel and created software to present information to team leaders</li>
+                  <li>Leveraged knowledge in Java, SQL, and Excel</li>
+                </ul>
+             </Card.Text>
+            </Card.Body>
+          </Card>
+          <Card bg="dark" text="white">
+            <Card.Header as="h3">Skils</Card.Header>
+            <Card.Body>
+              <Card.Text>
+              <h5>LANGUAGES/FRAMEWORKS</h5>
+                <ul>
+                  <li>Flutter and Dart</li>
+                  <li>Web stack: React, Javascript, Html, Css</li>
+                  <li>Languages: C++, Java, Ruby, C, Python</li>
+                </ul>
+                <h5>Technologies</h5>
+                <ul>
+                  <li>IDE's: VS Code, Intellij, Webstorm, Pycharm, Android Studio</li>
+                  <li>Google Firebase, AWS</li>
+                  <li>Design Architecutre: BLoC, Redux, MVC, MVVM</li>
+                </ul>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </CardDeck>
+        
   </Container>
+  </section>
   
   </body>
+  
+  <footer>
+    
+  </footer>
+  </div>
   );
 };
 

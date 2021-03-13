fb6bb4a9bf41ab7d0f8e534ef919663468041827
Rajdeep <60407196+Rajdeep0303@users.noreply.github.com>
2021-03-13 15:42:32 -0800

    Add files via upload

--- application/frontend/src/pages/Henzon.js
+++ application/frontend/src/pages/Henzon.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React from "react";
+import "../assets/css/about_individual.css";
+import ProfilePic from "../assets/img/about/henzon/henzon.jpg";
 
 const Henzon = () => {
   return (
-    <div>
-      <h1>Henzon</h1>
+    <div className="about-page">
+      <div className="jumbotron jumbotron-fluid about-header">
+        <div className="d-flex flex-wrap justify-content-center">
+          <div className="mx-5">
+            <img
+              className="rounded-circle about-img"
+              src={ProfilePic}
+              alt="profilepicture"
+            />
+          </div>
+          <div className="about-me-text pt-5 text-white">
+            <h1 className="text-warning">About Me</h1>
+            <h4>Henzon Zambrano</h4>
+            <p>
+              Undergraduate Student <br /> San Francisco State University
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };

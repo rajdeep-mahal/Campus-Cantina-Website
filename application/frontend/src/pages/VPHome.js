5cb796e787f8f0b2a6ad1bb8074d23333cd1d29a
rinaykumar <rkumar4@mail.sfsu.edu>
2021-03-15 14:55:40 -0700

    added google maps to search results

--- application/frontend/src/pages/VPHome.js
+++ application/frontend/src/pages/VPHome.js
@@ -1,12 +1,11 @@
-// TODO: Retrieve addresses and display with Google Maps API
-//       Validate search terms before sending to backend
+// TODO: Validate search terms before sending to backend
 //       Optional: Autocomplete / Suggestions in search bar
 //       Frontend styling
 //       QA testing
 
 import React from 'react';
 import axios from 'axios';
-
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 
 const VPHome = () => {
   const [searchResults, setSearchResults] = React.useState([]);
@@ -16,9 +15,11 @@ const VPHome = () => {
   const [noResult, setNoResult] = React.useState('');
 
   const handleSearch = () => {
-    axios.get('http://localhost:3001/search', {params: {searchTerm: searchTerm, category: selectedCategory}} )
+    axios
+      .get('http://localhost:3001/search', {
+        params: { searchTerm: searchTerm, category: selectedCategory },
+      })
       .then((res) => {
-        //console.log(JSON.stringify(res.data));
         if (res.data.length === 0) {
           setSearchResults([]);
           setNoResult('No results found.');
@@ -26,59 +27,98 @@ const VPHome = () => {
           setSearchResults(res.data);
           setNoResult('');
         }
-      })
+      });
   };
 
   const handleCategory = () => {
     let e = document.getElementById('categoryDropDown');
     setSelectedCategory(e.options[e.selectedIndex].value);
-  }
-  
+  };
+
   React.useEffect(() => {
-    axios.get('http://localhost:3001/categories')
-      .then((res) => {
-        //console.log(res.data);
-        setCategories(res.data)
-      })
-  }, [])
+    axios.get('http://localhost:3001/categories').then((res) => {
+      setCategories(res.data);
+    });
+  }, []);
 
   return (
-    <div style={{padding: '10px'}}>
-        <h1>VP Home</h1>
-        <h3>CSC 648 <br/> Spring 2021 <br/> Team 04</h3>
-        <a href='/about'><h5>About Page</h5></a>
-        <p style={{fontSize: '8px'}}>All images are free-use.</p>
-        <br/>
+    <div style={{ padding: '10px' }}>
+      <h1>VP Home</h1>
+      <h3>
+        CSC 648 <br /> Spring 2021 <br /> Team 04
+      </h3>
+      <a href="/about">
+        <h5>About Page</h5>
+      </a>
+      <p style={{ fontSize: '8px' }}>All images are free-use.</p>
+      <br />
 
-        <div style={{display: 'flex'}}>
-          {/* Category Dropdown List */}
-          <select name='categoryDropDown' id='categoryDropDown' onChange={handleCategory}>
-            <option value=''>All Categories</option>
-            {categories.map((category, i) => (
-              <option value={category.Category} key={i}>{category.Category}</option>
-            ))}
-          </select>
-          
-          {/* Search Bar */}
-          <input type='text' size='25' onChange={e => setSearchTerm(e.target.value)}></input>   
-          <button onClick={handleSearch}>Search</button>
-        </div> 
-
-        {/* Search Results */}
-        <br/>
-        <div>
-            {searchResults.map((item, i) => (
-              <div key={i} style={{border: '1px solid lightgrey', width: '406px', padding: '2px', marginBottom: '10px'}}>
-                <img src={'data:image/jpeg;base64,' + new Buffer(item.Pic1).toString('base64')}
-                  alt=''
-                  width='400px'
-                  height='250px'
-                />
-                <p><strong>{item.Name}</strong><br/>{item.Price_Level} • {item.Category}, {item.Tags}</p>
-              </div>
-            ))}
-            {noResult}
-        </div>
+      <div style={{ display: 'flex' }}>
+        {/* Category Dropdown List */}
+        <select
+          name="categoryDropDown"
+          id="categoryDropDown"
+          onChange={handleCategory}
+        >
+          <option value="">All Categories</option>
+          {categories.map((category, i) => (
+            <option value={category.Category} key={i}>
+              {category.Category}
+            </option>
+          ))}
+        </select>
+        {/* Search Bar */}
+        <input
+          type="text"
+          size="25"
+          onChange={(e) => setSearchTerm(e.target.value)}
+        ></input>
+        <button onClick={handleSearch}>Search</button>
+      </div>
+      {/* Search Results */}
+      <br />
+      <div>
+        {searchResults.map((item, i) => (
+          <div
+            key={i}
+            style={{
+              border: '1px solid lightgrey',
+              width: '406px',
+              padding: '2px',
+              marginBottom: '12px',
+            }}
+          >
+            <img
+              src={
+                'data:image/jpeg;base64,' +
+                new Buffer(item.Pic1).toString('base64')
+              }
+              alt=""
+              width="400px"
+              height="250px"
+            />
+            <p>
+              <strong>{item.Name}</strong>
+              <br />
+              {item.Price_Level} • {item.Category}, {item.Tags}
+            </p>
+            {/* Google Maps */}
+            <LoadScript
+              // **Dont push API Key to GitHub**
+              googleMapsApiKey='API KEY'
+            >
+              <GoogleMap
+                mapContainerStyle={{ height: '250px', width: '400px' }}
+                zoom={17}
+                center={{ lat: item.Lat, lng: item.Lng }}
+              >
+                <Marker position={{ lat: item.Lat, lng: item.Lng }} />
+              </GoogleMap>
+            </LoadScript>
+          </div>
+        ))}
+        {noResult}
+      </div>
     </div>
   );
 };

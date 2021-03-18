0e381d46fb68d769c7566e9554d9fabd08132738
henzonz <zhenzon26@gmail.com>
2021-03-17 21:54:15 -0700

    fixed my about page responsiveness (mobile)

--- application/frontend/src/pages/Henzon.js
+++ application/frontend/src/pages/Henzon.js
@@ -17,18 +17,18 @@ import RainbowReef from "../assets/img/about/henzon/rainbowreef_game.png"
 import Calculator from "../assets/img/about/henzon/calculator.png"
 import Hercules from "../assets/img/about/henzon/hercules-app.png"
 
-
-
-
-
 const Henzon = () => {
+  React.useLayoutEffect(() => {
+    window.scrollTo(0, 0);
+  });
+  
   return (
     <div className="about-page">
       <div className="jumbotron jumbotron-fluid about-header">
         <div className="d-flex flex-wrap justify-content-center">
           <div className="mx-5">
             <img
-              className="rounded-circle hz-about-img"
+              className="rounded-circle about-img"
               src={ProfilePic}
               alt="profilepicture"
             />
@@ -42,30 +42,32 @@ const Henzon = () => {
           </div>
         </div>
       </div>
-
-      <div className="hz-about-body container-fluid">
+      <div className="about-body container-fluid">
         <div className="about-desc card">
           <div
             className="card-header"
             style={{ backgroundColor: "rgb(172, 155, 223)" }}
           >
-            <a href = "https://www.linkedin.com/in/henzon-zambrano-06b116105/"><img class = "hz-contacts" src = {Linkedin} alt = ""></img></a>
-            <a href = "https://github.com/henzonz"><img class = "hz-contacts" src = {Github} alt = ""></img></a>
-            <a href = "mailto:henzon.zno26@gmail.com"><img class = "hz-contacts" src = {Mail} alt = ""></img></a>
-
+            <i className="fas fa-university mr-2" />
+            <i className="fas fa-server mr-2" />
+            <i className="fas fa-gamepad mr-2" />
+            <i className="fas fa-heart mr-2" />
           </div>
           <div className="card-body bg-light">
             <p>
               <i>
-                Hello! I'm Henzon and I'm an undergraduate student pursuing a B.S in Computer Engineering at SFSU. 
+              Hello! I'm Henzon and I'm an undergraduate student pursuing a B.S in Computer Engineering at SFSU.
                 <br />
                 I'm from the Philippines and moved to the U.S in 2007. I currently live in the beautiful city of San Francisco, California.
                 <br />
                 During my free time, I enjoy working out, watching tv shows/movies, photography and playing video games!
-                <br/>
+                <br />
                 I'm interested in both frontend and backend development. I mainly enjoy frontend development, but I'm also eager to learn backend development to experience both sides of software development and be more flexible.
               </i>
             </p>
+            <a href = "https://www.linkedin.com/in/henzon-zambrano-06b116105/"><img class = "hz-contacts" src = {Linkedin} alt = ""></img></a>
+            <a href = "https://github.com/henzonz"><img class = "hz-contacts" src = {Github} alt = ""></img></a>
+            <a href = "mailto:henzon.zno26@gmail.com"><img class = "hz-contacts" src = {Mail} alt = ""></img></a>
           </div>
         </div>
         <div>
@@ -75,65 +77,60 @@ const Henzon = () => {
               <h4 className="about-skill-name">HTML</h4>
               <img src={HTML} width="74" height="74" alt="" />
             </div>
-
             <div className="card about-skill-set mt-2">
               <h4 className="about-skill-name">CSS</h4>
               <img src={CSS} width="74" height="74" alt="" />
             </div>
-
             <div className="card about-skill-set mt-2">
               <h4 className="about-skill-name">Java</h4>
               <img src={Java} width="74" height="74" alt="" />
             </div>
-
             <div className="card about-skill-set mt-2">
               <h4 className="about-skill-name">C</h4>
               <img src={C} width="74" height="74" alt="" />
             </div>
-
             <div className="card about-skill-set mt-2">
               <h4 className="about-skill-name">Swift</h4>
               <img src={Swift} width="74" height="74" alt="" />
             </div>
-
             <div className="card about-skill-set mt-2">
               <h4 className="about-skill-name">C++</h4>
               <img src={Cpp} width="74" height="74" alt="" />
             </div>
+
           </div>
         </div>
         <div className="container">
           <h2 className="text-center pt-4">Recent Projects</h2>
           <div className="d-flex justify-content-around row text-center flex-wrap mt-3">
-            <div className="hz-about-projects card col-sm-6 col-lg-4 py-2 m-1">
+            <div className="about-projects card col-sm-6 col-lg-4 py-2 m-1">
               <h4 className="about-skill-name">
-               <a className = "hz-project-links" href = "https://github.com/henzonz/Super-Rainbow-Reef-Game"> <strong>Super Rainbow Reef Game</strong> </a>
+               <a className = "project-links" href = "https://github.com/henzonz/Super-Rainbow-Reef-Game"> <strong>Super Rainbow Reef Game</strong> </a>
               </h4>
 
               <blockquote className="card-body bg-light font-italic">
                 A simple 2D game created using Java to practice object oriented programming.
               </blockquote><br/>
-              <img src = {RainbowReef} alt = ""/>
+              <img className = "img-fluid card-img-bottom" src = {RainbowReef} width = "150" height = "150" alt = ""/>
             </div>
-
-            <div className="hz-about-projects card col-sm-6 col-lg-4 py-2 m-1">
+            <div className="about-projects card col-sm-6 col-lg-4 py-2 m-1">
               <h4 className="about-skill-name">
-               <a className = "hz-project-links" href = "https://github.com/henzonz/Calculator-GUI-asmt-"> <strong>Calculator</strong> </a>
+               <a className = "project-links" href = "https://github.com/henzonz/Calculator-GUI-asmt-"> <strong>Calculator</strong> </a>
               </h4>
 
               <blockquote className="card-body bg-light font-italic">
               A simple calculator GUI that properly evaluates mathematical expressions by following the correct order of operations.              </blockquote><br/>
-              <img src = {Calculator} alt = ""/>
+              <img className = "img-fluid card-img-bottom" src = {Calculator} width = "150" height = "150" alt = ""/>
             </div>
 
-            <div className="hz-about-projects card col-sm-6 col-lg-4 py-2 m-1">
+            <div className="about-projects card col-sm-6 col-lg-4 py-2 m-1">
               <h4 className="about-skill-name">
-               <a className = "hz-project-links text-decoration-none" href = "https://sites.google.com/view/fitapp-repcounter/design?authuser=0"> <strong>Hercules App</strong> </a>
+               <a className = "project-links text-decoration-none" href = "https://sites.google.com/view/fitapp-repcounter/design?authuser=0"> <strong>Hercules App</strong> </a>
               </h4>
 
-              <blockquote className="card-body bg-light font-italic">
+              <blockquote className="project-desc card-body bg-light font-italic">
               A simple fitness app rep counter that utilizes the three iPhone sensors: proximity sensor, gyroscope and touch screen. The app is created using the Swift language that allow users to workout without having to count repetitions of the mentioned workouts.              </blockquote><br/>
-              <img src = {Hercules} alt = ""/>
+              <img className = "img-fluid card-img-bottom" src = {Hercules} width = "150" height = "150" alt = ""/>
             </div>
           </div>
         </div>

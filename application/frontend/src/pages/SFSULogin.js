f6afcd964f3e5dae67f8c9bece9770239602bb1e
Rajdeep <60407196+Rajdeep0303@users.noreply.github.com>
2021-04-01 12:08:09 -0700

    Add files via upload

--- application/frontend/src/pages/SFSULogin.js
+++ application/frontend/src/pages/SFSULogin.js
@@ -1,14 +1,28 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const SFSULogin = () => {
-
   return (
-    <div className="text-center">
-      <br/>
-      <h1>SFSU Sign In</h1>
-      <Link to="/sfsusignup">Sign Up</Link>
-    </div>
+      <div className="login_container">
+          <form id="login-form" method="post" encType="application/x-www-form-urlencoded">
+              <input id="redirect-input" type="hidden" name="redirect" />
+              <fieldset className="login_fieldset-form">
+
+                  <legend className="login_page-title">Login</legend>
+
+                  <login_label>Email "@mail.sfsu.edu"</login_label>
+                  <input className="login_input-field" type="text" id="displayNameInput" name="displayName" placeholder="Email ''@mail.sfsu.edu'' " required />
+
+                  <login_label>Password</login_label>
+                  <input className="login_input-field" type="password" id="passwordInput" name="password" placeholder="Password" required />
+                  <br />
+
+                  <a href="/register" className="card-link">&nbsp;Forgot Password?</a>
+                  <Link to="/SFSUSignup">&nbsp;Don't have an account?</Link>
+                  <br />
+                  <button type="submit" className="login_button">Login</button>
+              </fieldset>
+          </form>
+      </div>
   );
 };
 

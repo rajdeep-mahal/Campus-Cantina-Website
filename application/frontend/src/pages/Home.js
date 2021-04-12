f542eaee1835ca7114cf2436e27259abc80742ab
henzonz <zhenzon26@gmail.com>
2021-04-11 19:59:54 -0700

    driver customer orders half way done.

--- application/frontend/src/pages/Home.js
+++ application/frontend/src/pages/Home.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "../assets/css/home.css";
+import DriverCustomerOrders from '../pages/DriverCustomerOrders'
 
 
 const Home = () => {
   return (
     <div className="home mt-4">
-     
+     <DriverCustomerOrders/>
     </div>
   );
 };

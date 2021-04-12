7a20689d060a78474e91937a122faf2d7fcde509
henzonz <zhenzon26@gmail.com>
2021-04-11 23:57:12 -0700

    finished design, need responsive work

--- application/frontend/src/pages/DriverCustomerOrders.js
+++ application/frontend/src/pages/DriverCustomerOrders.js
@@ -6,22 +6,38 @@ import map_sample from '../assets/img/map_customerorder.png';
 
 const DriverCustomerOrders = () =>{
     return(
-        <div className = "order_details p-5 m-5">
+        <div className = "container-fluid">
             <div className = "order_header h3 text-white text-center py-3">Order Details</div>
-            <div className = "h4 px-3 pt-3 font-italic font-weight-bold">Customer Order: <span className = "h4">#112233</span></div>
-            <div className = "customer_order_card card border border-warning">
+            <div className = "card card-fluid border border-warning w-75 d-flex mx-auto">
+            <div className = "card-header h4 pt-3 font-italic font-weight-bold bg-warning">Customer Order:<span className = "h4">#112233</span></div>
                 <div className = "card-body">
-                    <div className = "px-4 py-2">
+                    <div className = "py-2 px-5">
                     <div className = "h4 text-center font-weight-bold">Head to Customer</div>
-                    <img src = {map_sample} alt = "map_sample" className = "img-fluid customer_order_map pb-4" />
-                        <span className = "h5">Order Details</span>
+                    <img src = {map_sample} alt = "map_sample" className = "d-block img-fluid customer_order_map pb-4 ml-auto mr-auto" />
+                    <div className = "row justify-content-around">
+                        <div className = "col-4">
+                        <h5>Order Details</h5>
                     <ul className = "py-1">
                         <li>Sandwich</li>
                         <li>Coke</li>
                     </ul>
                     <div className = "h5 font-weight-bold">Total: <span>&#36;24</span></div>
-                    <div className = "h5">Order Status: <span className = "font-italic">Pending</span></div>
+                    <div className = "h5 pt-3">Order Status: <span className = "font-italic">Pending</span></div>
+                        </div>
+
+                        <div className = "col-3">
+                        <h5>Customer:</h5>
+                    <ul className = "list-unstyled">
+                        <li className = "px-2">Will Smith</li>
+                        <li className = "px-2">(415) 123-4567</li>
+                    </ul>
+                    <button type = "button" className = "btn btn-warning mb-2 ml-2 text-white">Directions</button>
+                    <div className = "h5">Delivery Instructions: <br/> *<span className = "font-italic">Our apartment is located at the back of the building</span>*</div>
+                        </div>
                     </div>
+                    <div className = "delivered_text text-white text-center w-25 h4">Delivered!</div>
+                    </div>
+
                 </div>
             </div>
 

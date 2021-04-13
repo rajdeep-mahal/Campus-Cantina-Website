17da837bdaa3188ed0a98053f4ab7db57399d84d
Bhavani Goruganthu <bhavani.goruganthu@mail.sfsu.edu>
2021-04-12 19:50:36 -0700

    Updated anchor tags with btn to remove warnings

--- application/frontend/src/components/CuisineRow.js
+++ application/frontend/src/components/CuisineRow.js
@@ -38,67 +38,88 @@ const CuisineRow = () => {
     <div className="container">
       <h2 className="cuisine-section-heading">Cuisines</h2>
       <div className=" d-flex flex-wrap cuisine-section mt-2">
-        <a
-          href="#"
-          onClick={() => handleCuisineSearch('Burgers')}
-          className="shadow cuisine-link"
-        >
-          <img src={Burger} alt="logo" height="55" className="" />
-          <p>Burgers</p>
-        </a>
-        <a
-          href="#"
-          onClick={() => handleCuisineSearch('Chinese')}
-          className="cuisine-link"
-        >
-          <img
-            src={Chinese}
-            alt="logo"
-            height="65"
-            className="chinese-cuisine"
-          />
-          <p>Chinese</p>
-        </a>
-        <a
-          href="#"
-          onClick={() => handleCuisineSearch('Indian')}
-          className="cuisine-link"
-        >
-          <img src={Indian} alt="logo" height="55" className="" />
-          <p>Indian</p>
-        </a>
-        <a
-          href="#"
-          onClick={() => handleCuisineSearch('Italian')}
-          className="cuisine-link"
-        >
-          <img src={Italian} alt="logo" height="55" className="" />
-          <p>Italian</p>
-        </a>
-        <a
-          href="#"
-          onClick={() => handleCuisineSearch('Mexican')}
-          className="cuisine-link"
-        >
-          <img src={Mexican} alt="logo" height="55" className="" />
-          <p>Mexican</p>
-        </a>
-        <a
-          href="#"
-          onClick={() => handleCuisineSearch('Pizza')}
-          className="cuisine-link"
-        >
-          <img src={Pizza} alt="logo" height="55" className="" />
-          <p>Pizza</p>
-        </a>
-        <a
-          href="#"
-          onClick={() => handleCuisineSearch('Vietnamese')}
-          className="cuisine-link"
-        >
-          <img src={Vietnamese} alt="logo" height="55" className="" />
-          <p>Vietnamese</p>
-        </a>
+        <div className="cuisine-link">
+          <button
+            type="button"
+            className="link-button"
+            onClick={() => handleCuisineSearch('Burgers')}
+          >
+            <img src={Burger} alt="logo" height="55" className="" />
+            <br />
+            Burgers
+          </button>
+        </div>
+        <div className="cuisine-link">
+          <button
+            type="button"
+            className="link-button"
+            onClick={() => handleCuisineSearch('Chinese')}
+          >
+            <img
+              src={Chinese}
+              alt="logo"
+              height="65"
+              className="chinese-cuisine"
+            />
+            <br />
+            Chinese
+          </button>
+        </div>
+        <div className="cuisine-link">
+          <button
+            type="button"
+            className="link-button"
+            onClick={() => handleCuisineSearch('Indian')}
+          >
+            <img src={Indian} alt="logo" height="55" className="" />
+            <br />
+            Indian
+          </button>
+        </div>
+        <div className="cuisine-link">
+          <button
+            type="button"
+            className="link-button"
+            onClick={() => handleCuisineSearch('Italian')}
+          >
+            <img src={Italian} alt="logo" height="55" className="" />
+            <br />
+            Italian
+          </button>
+        </div>
+        <div className="cuisine-link">
+          <button
+            type="button"
+            className="link-button"
+            onClick={() => handleCuisineSearch('Mexican')}
+          >
+            <img src={Mexican} alt="logo" height="55" className="" />
+            <br />
+            Mexican
+          </button>
+        </div>
+        <div className="cuisine-link">
+          <button
+            type="button"
+            className="link-button"
+            onClick={() => handleCuisineSearch('Pizza')}
+          >
+            <img src={Pizza} alt="logo" height="55" className="" />
+            <br />
+            Pizza
+          </button>
+        </div>
+        <div className="cuisine-link">
+          <button
+            type="button"
+            className="link-button"
+            onClick={() => handleCuisineSearch('Vietnamese')}
+          >
+            <img src={Vietnamese} alt="logo" height="55" className="" />
+            <br />
+            Vietnamese
+          </button>
+        </div>
       </div>
     </div>
   );

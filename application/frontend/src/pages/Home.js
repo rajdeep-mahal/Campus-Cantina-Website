19e5c1d5ff2e3138c6c4f2c51d0355f592cae6da
henzonz <zhenzon26@gmail.com>
2021-04-14 19:10:29 -0700

    testing branch

--- application/frontend/src/pages/Home.js
+++ application/frontend/src/pages/Home.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "../assets/css/home.css";
-
+import DriverAvailableOrders from './DriverAvailableOrders.js'
 
 const Home = () => {
   return (
     <div className="home mt-4">
-     
+     <DriverAvailableOrders/>
     </div>
   );
 };

b3cd3c64d75f28802914d66658f4e5b5875c72a6
henzonz <zhenzon26@gmail.com>
2021-04-17 12:20:15 -0700

    saving.. fixed responsiveness but still needs work.

--- application/frontend/src/pages/DriverOrderDelivered.js
+++ application/frontend/src/pages/DriverOrderDelivered.js
@@ -7,32 +7,36 @@ import map_sample from '../assets/img/map_customerorder.png';
 const DriverOrderDelivered = () =>{
     return(
         <div className = "container-fluid">
-            <div className = "order_header h3 text-white text-center py-3">Order Details</div>
-            <div className = "card card-fluid border border-warning w-75 d-flex mx-auto">
-            <div className = "card-header h4 pt-3 font-italic font-weight-bold bg-warning">Customer Order:<span className = "h4">#112233</span></div>
+            <div className = "order_header h3 text-white text-center mx-auto py-2 w-50 mb-5">Order Details</div>
+            <div className = "card border card_customerorder_body w-75 d-flex mx-auto mb-5">
+            <div className = "card-header card_customerorder h4 pt-3 font-italic font-weight-bold text-white">Customer Order:<span className = "h4">#112233</span></div>
                 <div className = "card-body">
                     <div className = "py-2 px-5">
-                    <div className = "h4 text-center font-weight-bold">Head to Customer</div>
-                    <img src = {map_sample} alt = "map_sample" className = "border border-warning d-block img-fluid customer_order_map pb-4 ml-auto mr-auto mb-4" />
+                    <div className = "h3 text-center font-weight-bold">Head to Customer</div>
+                    <img src = {map_sample} alt = "map_sample" className = "border d-block img-fluid customer_order_map pb-4 ml-auto mr-auto mb-4" />
                     <div className = "row justify-content-around">
                         <div className = "col-4">
-                        <h5>Order Details</h5>
-                    <ul className = "py-1">
-                        <li>Sandwich</li>
-                        <li>Coke</li>
+                            <div className = "row">
+                        <div className = "h4">Order Details</div>
+                    <ul>
+                        <br/><li className = "items">Sandwich</li>
+                        <li className = "items">Coke</li>
                     </ul>
-                    <div className = "h5 font-weight-bold">Total: <span>&#36;24</span></div>
-                    <div className = "h5 pt-3">Order Status: <span className = "font-italic">Pending</span></div>
+                        </div>
+                        <div className = "row h5 font-weight-bold pt-4">Total: <span> &#36;24</span></div>
+                        <div className = "row h5 pt-3 pt-4">Order Status: <span className = "font-italic text-info font-weight-bold">Pending</span></div>
                         </div>
 
                         <div className = "col-3">
-                        <h5>Customer:</h5>
-                    <ul className = "list-unstyled">
-                        <li className = "px-2">Will Smith</li>
-                        <li className = "px-2">(415) 123-4567</li>
-                    </ul>
-                    <button type = "button" className = "btn btn-warning mb-2 ml-2 text-white">Directions</button>
-                    <div className = "h5">Delivery Instructions: <br/> *<span className = "font-italic">Our apartment is located at the back of the building</span>*</div>
+                            <div className = "row">
+                                <div className = "h4">Customer:</div>
+                                <ul className = "customer_info"> <br/>
+                                    <li className = "items px-2">Will Smith</li>
+                                    <li className = "items px-2">(415) 123-4567</li>
+                                </ul>
+                            </div>
+                            <button type = "button" className = "row btn btn-warning mb-2 ml-2 text-white mt-4">Directions</button>
+                            <div className = "delivery_instructions h5 pt-4 row">Delivery Instructions: <br/> *<span className = "font-italic">Our apartment is located at the back of the building</span>*</div>
                         </div>
                     </div>
                     <div className = "delivered_text text-white text-center w-25 h4">Delivered!</div>
--- application/frontend/src/pages/Home.js
+++ application/frontend/src/pages/Home.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "../assets/css/home.css";
+import DriverOrderDelivered from './DriverOrderDelivered'
 
 
 
 const Home = () => {
   return (
     <div className="home mt-4">
-    
+    <DriverOrderDelivered/>
     </div>
   );
 };

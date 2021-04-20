2b7c2dbd5d34e844bf0f480af1ec15e1d3b13722
white <white.freddie95@gmail.com>
2021-04-20 14:38:48 -0700

    added new sidebars

---
+++ application/frontend/src/components/DriverMenuItem.js
@@ -0,0 +1,18 @@
+export const DriverMenuItems = [
+    {
+      title: 'Current Order',
+      path: '/driveravailableorders',
+      cName: 'primary-color fas fa-utensils',
+    },
+    {
+      title: 'Order History',
+      path: '/',
+      cName: 'primary-color fas fa-history',
+    },
+    {
+      title: 'Sign Out',
+      path: '/',
+      cName: 'primary-color fas fa-sign-out-alt',
+    },
+    
+  ];
---
+++ application/frontend/src/components/OwnerMenuItem.js
@@ -0,0 +1,22 @@
+export const OwnerMenuItems = [
+    {
+      title: 'Home',
+      path: '/owner/profile',
+      cName: 'primary-color fas fa-home',
+    },
+    {
+      title: 'Profile',
+      path: '/owner/menu',
+      cName: 'primary-color fas fa-user-circle',
+    },
+    {
+      title: 'Orders',
+      path: '/owner/orders',
+      cName: 'primary-color fas fa-utensils',
+    },
+    {
+      title: 'Sign out',
+      path: '/',
+      cName: 'primary-color fas fa-sign-out-alt',
+    },
+  ];

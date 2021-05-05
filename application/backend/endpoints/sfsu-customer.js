1dc2bc60c56756f7674d0e9e2ce27fd2e117b9b4
rinaykumar <rkumar4@mail.sfsu.edu>
2021-05-04 22:32:28 -0700

    added driver and customer signup endpoints

--- application/backend/endpoints/sfsu-customer.js
+++ application/backend/endpoints/sfsu-customer.js
@@ -3,26 +3,58 @@
 const express = require('express');
 const router = express.Router();
 const database = require('../db');
-const validator = require('validator');   // Used for input validation
+const validator = require('validator'); // Used for input validation
+const bcrypt = require('bcryptjs');
 
+router.use(express.json());
+
+// Testing endpoint
 router.get('/', (req, res) => {
-    res.send('SFSU Customer endponts')
-})
+  res.send('SFSU Customer endponts');
+});
 
-// API call to get all SFSU customers
+// Call to get all SFSU customers for testing
 router.get('/all-sfsu-customers', (req, res) => {
-    database.query('SELECT * FROM SFSU_Customers', (err, result) => {
-        console.log('Called all-sfsu-customers endpoint');
-        res.send(result);
-      });
+  database.query('SELECT * FROM SFSU_Customers', (err, result) => {
+    console.log('Called all-sfsu-customers endpoint');
+    res.send(result);
+  });
 });
 
 // Customer sign up
+router.post('/customer-signup', (req, res) => {
+  console.log('Called customer-singnup endpoint');
+
+  // TODO: Validate driver data
+
+  // Encyprt password
+  let hash = bcrypt.hashSync(req.body.customerPassword, 10);
+
+  // Generate SQL query with driver info
+  let query =
+    `INSERT INTO SFSU_Customers VALUES (` +
+    req.body.customerID +
+    `,'` +
+    req.body.customerName +
+    `','` +
+    req.body.customerAddress +
+    `','` +
+    req.body.customerType +
+    `','` +
+    req.body.customerPhone +
+    `','` +
+    req.body.customerEmail +
+    `','` +
+    hash +
+    `', 'Active')`; // Status set to active, admin can deactivate
+
+  // Send driver query to db
+  database.query(query, (err, result) => {
+    console.log('Uploaded customer info to db');
+    res.send(result);
+  });
+});
 
 // Customer log in
-
-// Get customer order history
-
-// Get customer addresses
 
 module.exports = router;

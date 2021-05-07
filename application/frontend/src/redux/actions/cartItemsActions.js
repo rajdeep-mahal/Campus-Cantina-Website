7917a2ab458a624981b750fbbd198e93806f0643
Bhavani Goruganthu <bhavani.goruganthu@mail.sfsu.edu>
2021-05-06 18:48:58 -0700

    cart Items Redux initial setup

---
+++ application/frontend/src/redux/actions/cartItemsActions.js
@@ -0,0 +1,19 @@
+export const setCartItems = (cartItems) => ({
+  type: 'CART_ITEMS_SET',
+  cartItems,
+});
+
+export const setCartItemsTotalCount = (cartItemsTotalCount) => ({
+  type: 'CART_ITEMS_TOTAL_COUNT_SET',
+  cartItemsTotalCount,
+});
+
+export const setCartTotal = (cartTotal) => ({
+  type: 'CART_TOTAL_SET',
+  cartTotal,
+});
+
+export const setCartDeliveryInstructions = (cartDeliveryInstructions) => ({
+  type: 'CART_DELIVERY_INSTRUCTIONS_SET',
+  cartDeliveryInstructions,
+});

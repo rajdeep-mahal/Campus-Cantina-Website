e1c6be523c2a53a80c96e7fc1bfb2cfb60e9ced1
Bhavani Goruganthu <bhavani.goruganthu@mail.sfsu.edu>
2021-05-07 20:01:22 -0700

    redux setup for logged in user

--- application/frontend/src/redux/reducers/rootReducer.js
+++ application/frontend/src/redux/reducers/rootReducer.js
@@ -3,10 +3,12 @@ import { combineReducers } from 'redux';
 import searchReducer from './searchReducer';
 import ownerSignupReducer from './ownerSignupReducer';
 import cartItemsReducer from './cartItemsReducer';
+import userReducer from './userReducer';
 
 export default combineReducers({
   searchReducer,
   ownerSignupReducer,
   cartItemsReducer,
+  userReducer,
   // TODO: Add more reducers
 });

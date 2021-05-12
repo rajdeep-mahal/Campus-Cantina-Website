22a657bbec8b4db148d365523f5311f4b4d05e5f
Bhavani Goruganthu <bhavani.goruganthu@mail.sfsu.edu>
2021-05-11 22:39:05 -0700

    alert for invalid suffix and bcrypt for pwd compare

--- application/frontend/src/pages/SFSULogin.js
+++ application/frontend/src/pages/SFSULogin.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import '../assets/css/login_Signup.css';
 import { Link, useHistory } from 'react-router-dom';
 import axios from 'axios';
-
 import bcrypt from 'bcryptjs';
 
 const SFSULogin = () => {
@@ -10,8 +9,10 @@ const SFSULogin = () => {
   const [customerEmail, setCustomerEmail] = useState('');
   const [customerPassword, setCustomerPassword] = useState('');
 
-  const onSubmitCustomerLogin = (event) => {
-    event.preventDefault();
+  // show error alert for invalid email suffix
+  const [showInvalidSuffixAlert, setShowInvalidSuffixAlert] = useState(false);
+
+  const loginCustomer = () => {
     axios
       .get('http://localhost:3001/api/sfsucustomer/customer-info', {
         params: {
@@ -26,69 +27,91 @@ const SFSULogin = () => {
             if (err) {
               throw err;
             } else if (!isMatch) {
-              console.log("Password doesn't match!");
+              //Password doesn't match!
+              setShowInvalidEmailAlert(false);
+              setShowInvalidPasswordAlert(true);
             } else {
-              console.log('Password matches!');
+              // 'Password matches!'
+              setShowInvalidEmailAlert(false);
+              setShowInvalidPasswordAlert(false);
               history.push('/');
             }
           }
         );
+      })
+      .catch((err) => {
+        setShowInvalidEmailAlert(true);
+        setShowInvalidPasswordAlert(false);
       });
   };
 
   return (
     <div className="login-container d-flex align-items-center justify-content-center">
-      <form
-        className="signup-signin-form"
-        method="post"
-        encType="application/x-www-form-urlencoded"
-        onSubmit={onSubmitCustomerLogin}
-      >
-        <div className="m-3">
-          <input id="redirect-input" type="hidden" name="redirect" />
-          <h2 className="font-weight-bold primary-color text-center">Login</h2>
-          <label htmlFor="Email" className="login-label">
-            Email
-          </label>
-          <input
-            className="login_input-field"
-            type="email"
-            id="sfsuEmail"
-            name="sfsuEmail"
-            placeholder="e.g. john.doe@mail.sfsu.edu"
-            value={customerEmail}
-            onChange={(e) => setCustomerEmail(e.target.value)}
-            onBlur={(e) => {
-              let temp = customerEmail.trim;
-            }}
-            required
-          />
-          <label htmlFor="Password" className="login-label">
-            Password
-          </label>
-          <input
-            className="login_input-field"
-            type="password"
-            id="passwordInput"
-            name="password"
-            placeholder="must have atleast 6 characters"
-            required
-            value={customerPassword}
-            onChange={(e) => setCustomerPassword(e.target.value)}
-          />
-          <br />
-          <br />
-          <a href="/">Forgot Password?</a> <br />
-          <Link to="/SFSUSignup">Don't have an account?</Link> <br />
-          <br />
-          <button
-            type="submit"
-            className="login_button d-flex align-items-center justify-content-center"
+      <div>
+        {showInvalidSuffixAlert ? (
+          <div
+            className="text-center mx-auto mt-2 alert alert-danger fade show w-100"
+            role="alert"
           >
-            Login
-          </button>
-        </div>
-      </form>
+            <b>Please enter a valid SFSU email address to continue.</b> <br />{' '}
+            <i>Example: john.doe@mail.sfsu.edu</i>
+          </div>
+        ) : (
+          <> </>
+        )}
+
+        <form
+          className="signup-signin-form mx-auto"
+          method="post"
+          encType="application/x-www-form-urlencoded"
+          onSubmit={onSubmitCustomerLogin}
+        >
+          <div className="m-3">
+            <input id="redirect-input" type="hidden" name="redirect" />
+            <h2 className="font-weight-bold primary-color text-center">
+              Login
+            </h2>
+            <label htmlFor="Email" className="login-label">
+              Email
+            </label>
+            <input
+              className="login_input-field"
+              type="email"
+              id="sfsuEmail"
+              name="sfsuEmail"
+              placeholder="e.g. john.doe@mail.sfsu.edu"
+              value={customerEmail}
+              onChange={(e) => setCustomerEmail(e.target.value)}
+              onBlur={onShowEmailError}
+              required
+            />
+            <label htmlFor="Password" className="login-label">
+              Password
+            </label>
+            <input
+              className="login_input-field"
+              type="password"
+              id="passwordInput"
+              name="password"
+              placeholder="must have atleast 6 characters"
+              required
+              value={customerPassword}
+              onChange={(e) => setCustomerPassword(e.target.value)}
+            />
+            <br />
+            <br />
+            <a href="/">Forgot Password?</a> <br />
+            <Link to="/SFSUSignup">Don't have an account?</Link> <br />
+            <br />
+            <button
+              type="submit"
+              className="login_button d-flex align-items-center justify-content-center"
+            >
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };

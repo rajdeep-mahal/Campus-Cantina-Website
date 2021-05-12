ae24197283ef7e04420ec40c2c65c8ffe8e7001a
Bhavani Goruganthu <bhavani.goruganthu@mail.sfsu.edu>
2021-05-11 21:10:04 -0700

    updated axios call

--- application/frontend/src/pages/SFSULogin.js
+++ application/frontend/src/pages/SFSULogin.js
@@ -1,14 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../assets/css/login_Signup.css';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import axios from 'axios';
+
+import bcrypt from 'bcryptjs';
 
 const SFSULogin = () => {
+  const history = useHistory();
+  const [customerEmail, setCustomerEmail] = useState('');
+  const [customerPassword, setCustomerPassword] = useState('');
+
+  const onSubmitCustomerLogin = (event) => {
+    event.preventDefault();
+    axios
+      .get('http://localhost:3001/api/sfsucustomer/customer-info', {
+        params: {
+          customerEmail: customerEmail,
+        },
+      })
+      .then((res) => {
+        bcrypt.compare(
+          customerPassword,
+          res.data[0].Password,
+          (err, isMatch) => {
+            if (err) {
+              throw err;
+            } else if (!isMatch) {
+              console.log("Password doesn't match!");
+            } else {
+              console.log('Password matches!');
+              history.push('/');
+            }
+          }
+        );
+      });
+  };
+
   return (
     <div className="login-container d-flex align-items-center justify-content-center">
       <form
         className="signup-signin-form"
         method="post"
         encType="application/x-www-form-urlencoded"
+        onSubmit={onSubmitCustomerLogin}
       >
         <div className="m-3">
           <input id="redirect-input" type="hidden" name="redirect" />
@@ -18,10 +52,15 @@ const SFSULogin = () => {
           </label>
           <input
             className="login_input-field"
-            type="text"
-            id="displayNameInput"
-            name="displayName"
+            type="email"
+            id="sfsuEmail"
+            name="sfsuEmail"
             placeholder="e.g. john.doe@mail.sfsu.edu"
+            value={customerEmail}
+            onChange={(e) => setCustomerEmail(e.target.value)}
+            onBlur={(e) => {
+              let temp = customerEmail.trim;
+            }}
             required
           />
           <label htmlFor="Password" className="login-label">
@@ -34,6 +73,8 @@ const SFSULogin = () => {
             name="password"
             placeholder="must have atleast 6 characters"
             required
+            value={customerPassword}
+            onChange={(e) => setCustomerPassword(e.target.value)}
           />
           <br />
           <br />

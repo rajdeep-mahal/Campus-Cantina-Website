6ee46f6f74374f47828ffafba087f3c65cde4b9c
henzonz <zhenzon26@gmail.com>
2021-05-16 02:23:36 -0700

    axios mapping done, this page will grab orders if Completed == 0. 2 Console errors.

--- application/frontend/src/pages/Driver/CurrentOrders.js
+++ application/frontend/src/pages/Driver/CurrentOrders.js
@@ -12,7 +12,7 @@ const CurrentOrders = () => {
     axios
       .get('http://localhost:3001/api/order/user-orders', {
         params: {
-          driverID: 1,
+          driverID: 586,
         },
       })
       .then((res) => {
@@ -21,91 +21,104 @@ const CurrentOrders = () => {
   }, []);
 
   return (
-    <div className="container-fluid">
-      <div className="order_header h3 text-white text-center mx-auto py-2 w-50">
-        Current Orders
-      </div>
-      <div className="card border card_customerorder_body w-75 d-flex mx-auto mb-5">
-        {orders.map((item, i) => (
+      <div>
+      {orders.filter((order) => order.Completed == 0).map((item,i) => (
+
+      
+    <div className="container">
+      <div className="card border card_customerorder_body mx-auto mt-3 mb-3">
+       
           <div className="card-header card_customerorder h4 pt-3 font-italic font-weight-bold text-white">
             Customer Order:
             <span className="h4 pl-2">#{item.ID}</span>
           </div>
-        ))}
+       
         <div className="card-body mx-auto">
-          <div className="py-1 px-5">
             <div className="h3 text-center font-weight-bold current-order-text">
               Head to Customer
-            </div>
-            <div className="text-center">
+              <div>
               {' '}
               <MyMap></MyMap>{' '}
             </div>
+            </div>
             <div className="container text-center">
               <div className="row">
-                <div className="col">
+                <div className="left-col col-5 col-sm-6">
                   <div className="pt-4">
-                    <span className="current-order-text h5">Order Details</span>
-                    {orders.map((item, i) => (
+                    <span className="current-order-text h5 font-weight-bold">Order Details</span>
+                    
                       <ul>
                         <div>{item.Restaurant_Name}</div>
                       </ul>
-                    ))}
+                    
                   </div>
                   <div className="pt-4">
                     {' '}
-                    <span className="current-order-text h5">Total</span>
-                    {orders.map((item, i) => (
+                    <span className="current-order-text font-weight-bold h5">Total</span>
+                    
                       <ul>
-                        <div>{item.Total}</div>
+                        <div>&#36;{item.Total}</div>
                       </ul>
-                    ))}
+                 
                   </div>
 
                   <div className="pt-4">
-                    <span className="current-order-text h5">Order Status:</span>
+                    <span className="current-order-text font-weight-bold h5">Order Status:</span>
                     <div className="pl-2 font-italic text-info">Pending</div>
                   </div>
                 </div>
 
-                <div className="col">
+                <div className="right-col col-5 col-sm-6">
+                <div className="pt-4">
+                    <span className="current-order-text font-weight-bold h5">
+                      Order No.
+                    </span>{' '}
+                    
+                      <ul>
+                        <div>#{item.ID}</div>
+                      </ul>
+                 
+                  </div>
                   <div className="pt-4">
-                    <span className="current-order-text h5">
+                    <span className="current-order-text font-weight-bold h5">
                       Customer Info:
                     </span>{' '}
-                    {orders.map((item, i) => (
+                    
                       <ul>
                         <div>{item.Customer_Name}</div>
                       </ul>
-                    ))}
+                  
                   </div>
-                  <div className="pt-4">
-                    <span className="current-order-text h5">
+                  <div className="pt-2">
+                    <span className="current-order-text font-weight-bold h5">
                       Delivery Instructions:
                     </span>
-                    {orders.map((item, i) => (
+                    
                       <ul>
                         <div>{item.Delivery_Instruction}</div>
                       </ul>
-                    ))}
+                   
                   </div>
                 </div>
               </div>
             </div>
 
-            <div className="click_delivered text-center mt-5 mb-2 text-muted">
+            <div className="click_delivered text-center col-md-12 mt-2 mb-2 text-muted">
               *Click delivered once food is handed to customer*
             </div>
             <button
               type="button"
-              className="col-9 col-sm-5 col-md-4 mx-auto delivered_button btn btn-block text-white"
-              onclick="myDirections()"
+              className="col-sm-12 col-4 py-2 btn btn-block delivered_button mx-auto text-white"
+              onClick="myDirections()"
             >
               Delivered!
             </button>
-          </div>
+          
         </div>
       </div>
+    </div>
+    
+    ))}
     </div>
   );
 };

cc004b6d38551ce7ced72d9b9a03c0e32299ff9c
Bhavani Goruganthu <bhavani.goruganthu@mail.sfsu.edu>
2021-05-16 17:19:10 -0700

    added route protection for owner & driver pages

--- application/frontend/src/pages/Driver/CurrentOrders.js
+++ application/frontend/src/pages/Driver/CurrentOrders.js
@@ -9,6 +9,8 @@ import React, { useState, useEffect } from 'react';
 
 const CurrentOrders = () => {
   const [orders, setOrders] = useState([]);
+  // redux global variable
+  const appUser = useSelector((state) => state.appUserReducer.appUser);
   const [orderStatus, setOrderStatus] = useState(0);
   const [modalItems, setModalItems] = useState([]);
 
@@ -25,160 +27,182 @@ const CurrentOrders = () => {
   }, []);
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/api/order/user-orders', {
-        params: {
-          driverID: 586,
-        },
-      })
-      .then((res) => {
-        setOrders(res.data);
-      });
+    if (appUser.type === 'driver') {
+      axios
+        .get('http://localhost:3001/api/driver/driver-info', {
+          params: {
+            driverEmail: appUser.email,
+          },
+        })
+        .then((res) => {
+          axios
+            .get('http://localhost:3001/api/order/user-orders', {
+              params: {
+                driverID: 586,
+              },
+            })
+            .then((res) => {
+              setOrders(res.data);
+            });
+        });
+    }
   }, []);
 
   return (
     <>
-      {orders
-        .filter((order) => order.Completed == 0)
-        .map((item, i) => (
-          <div className="container">
-            <div className="card border card_customerorder_body mx-auto mt-3 mb-3">
-              <div className="card-header card_customerorder h4 pt-3 font-italic font-weight-bold text-white">
-                Customer Order:
-                <span className="h4 pl-2">#{item.ID}</span>
-              </div>
-
-              <div className="card-body mx-auto">
-                <div className="h3 text-center font-weight-bold current-order-text">
-                  Head to Customer
-                  <div>
-                    {' '}
-                    <MyMap></MyMap>{' '}
+      {appUser.type === 'driver' ? (
+        <>
+          {orders
+            .filter((order) => order.Completed == 0)
+            .map((item, i) => (
+              <div className="container">
+                <div className="card border card_customerorder_body mx-auto mt-3 mb-3">
+                  <div className="card-header card_customerorder h4 pt-3 font-italic font-weight-bold text-white">
+                    Customer Order:
+                    <span className="h4 pl-2">#{item.ID}</span>
                   </div>
-                </div>
-                <div className="container text-center">
-                  <div className="row">
-                    <div className="left-col col-5 col-sm-6">
-                      <div className="pt-4">
-                        <span className="current-order-text h5 font-weight-bold">
-                          Order Details
-                        </span>
 
-                        <ul>
-                          <div>{item.Restaurant_Name}</div>
-                        </ul>
-                      </div>
-                      <div className="pt-4">
+                  <div className="card-body mx-auto">
+                    <div className="h3 text-center font-weight-bold current-order-text">
+                      Head to Customer
+                      <div>
                         {' '}
-                        <span className="current-order-text font-weight-bold h5">
-                          Total
-                        </span>
-                        <ul>
-                          <div>&#36;{item.Total}</div>
-                        </ul>
+                        <MyMap></MyMap>{' '}
                       </div>
+                    </div>
+                    <div className="container text-center">
+                      <div className="row">
+                        <div className="left-col col-5 col-sm-6">
+                          <div className="pt-4">
+                            <span className="current-order-text h5 font-weight-bold">
+                              Order Details
+                            </span>
 
-                      <div className="pt-4">
-                        <span className="current-order-text font-weight-bold h5">
-                          Order Status:
-                        </span>
-                        <div className="pl-2 font-italic text-info">
-                          Pending
+                            <ul>
+                              <div>{item.Restaurant_Name}</div>
+                            </ul>
+                          </div>
+                          <div className="pt-4">
+                            {' '}
+                            <span className="current-order-text font-weight-bold h5">
+                              Total
+                            </span>
+                            <ul>
+                              <div>&#36;{item.Total}</div>
+                            </ul>
+                          </div>
+
+                          <div className="pt-4">
+                            <span className="current-order-text font-weight-bold h5">
+                              Order Status:
+                            </span>
+                            <div className="pl-2 font-italic text-info">
+                              Pending
+                            </div>
+                          </div>
                         </div>
+
+                        <div className="right-col col-5 col-sm-6">
+                          <div className="pt-4">
+                            <span className="current-order-text font-weight-bold h5">
+                              Order No.
+                            </span>{' '}
+                            <ul>
+                              <div>#{item.ID}</div>
+                            </ul>
+                          </div>
+                          <div className="pt-4">
+                            <span className="current-order-text font-weight-bold h5">
+                              Customer Info:
+                            </span>{' '}
+                            <ul>
+                              <div>{item.Customer_Name}</div>
+                            </ul>
+                          </div>
+                          <div className="pt-2">
+                            <span className="current-order-text font-weight-bold h5">
+                              Delivery Instructions:
+                            </span>
+
+                            <ul>
+                              <div>{item.Delivery_Instruction}</div>
+                            </ul>
+                          </div>
+                        </div>
+                        <button
+                          type="button"
+                          className="col-9 col-sm-5 col-md-4 mx-auto delivered_button btn btn-block text-white"
+                          onclick="myDirections()"
+                        >
+                          Delivered!
+                        </button>
                       </div>
                     </div>
 
-                    <div className="right-col col-5 col-sm-6">
-                      <div className="pt-4">
-                        <span className="current-order-text font-weight-bold h5">
-                          Order No.
-                        </span>{' '}
-                        <ul>
-                          <div>#{item.ID}</div>
-                        </ul>
-                      </div>
-                      <div className="pt-4">
-                        <span className="current-order-text font-weight-bold h5">
-                          Customer Info:
-                        </span>{' '}
-                        <ul>
-                          <div>{item.Customer_Name}</div>
-                        </ul>
-                      </div>
-                      <div className="pt-2">
-                        <span className="current-order-text font-weight-bold h5">
-                          Delivery Instructions:
-                        </span>
-
-                        <ul>
-                          <div>{item.Delivery_Instruction}</div>
-                        </ul>
-                      </div>
+                    <div className="click_delivered text-center col-md-12 mt-2 mb-2 text-muted">
+                      *Click delivered once food is handed to customer*
                     </div>
                     <button
                       type="button"
-                      className="col-9 col-sm-5 col-md-4 mx-auto delivered_button btn btn-block text-white"
-                      onclick="myDirections()"
+                      className="col-sm-12 col-4 py-2 btn btn-block delivered_button mx-auto text-white"
+                      data-toggle="modal"
+                      data-target="#CompletedOrder"
+                      onClick={(e) => {
+                        orders
+                          .filter((order) => order.ID === item.ID)
+                          .map((item, i) => {
+                            setModalItems(JSON.parse(item.ID));
+                            setOrderStatus();
+                          });
+                      }}
                     >
                       Delivered!
                     </button>
                   </div>
                 </div>
-
-                <div className="click_delivered text-center col-md-12 mt-2 mb-2 text-muted">
-                  *Click delivered once food is handed to customer*
-                </div>
-                <button
-                  type="button"
-                  className="col-sm-12 col-4 py-2 btn btn-block delivered_button mx-auto text-white"
-                  data-toggle="modal"
-                  data-target="#CompletedOrder"
-                  onClick={(e) => {
-                    orders
-                      .filter((order) => order.ID === item.ID)
-                      .map((item, i) => {
-                        setModalItems(JSON.parse(item.ID));
-                        setOrderStatus();
-                      });
-                  }}
+                <div
+                  className="modal fade"
+                  id="CompletedOrder"
+                  tabIndex="-1"
+                  role="dialog"
+                  aria-labelledby="viewOrderTitle"
+                  aria-hidden="true"
                 >
-                  Delivered!
-                </button>
-              </div>
-            </div>
-            <div
-              className="modal fade"
-              id="CompletedOrder"
-              tabIndex="-1"
-              role="dialog"
-              aria-labelledby="viewOrderTitle"
-              aria-hidden="true"
-            >
-              <div className="modal-dialog" role="document">
-                <div className="modal-content">
-                  <div className="modal-header">
-                    <h5 className="modal-title" id="viewOrderTitle">
-                      Order Details
-                    </h5>
-                    <button
-                      type="button"
-                      className="close"
-                      data-dismiss="modal"
-                      aria-label="Close"
-                    >
-                      <span aria-hidden="true">&times;</span>
-                    </button>
-                  </div>
-                  <div className="modal-body">
-                    <div className="h4 text-center m-5">
-                      Delivery completed! Thank you!
+                  <div className="modal-dialog" role="document">
+                    <div className="modal-content">
+                      <div className="modal-header">
+                        <h5 className="modal-title" id="viewOrderTitle">
+                          Order Details
+                        </h5>
+                        <button
+                          type="button"
+                          className="close"
+                          data-dismiss="modal"
+                          aria-label="Close"
+                        >
+                          <span aria-hidden="true">&times;</span>
+                        </button>
+                      </div>
+                      <div className="modal-body">
+                        <div className="h4 text-center m-5">
+                          Delivery completed! Thank you!
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </div>
-        ))}
+            ))}
+        </>
+      ) : appUser.type === 'guest' ||
+        appUser.type === undefined ||
+        appUser.type === 'customer' ? (
+        <Redirect to="/" />
+      ) : appUser.type === 'owner' ? (
+        <Redirect to="/owner/menu" />
+      ) : (
+        <> </>
+      )}
     </>
   );
 };
--- application/frontend/src/pages/Owner/OwnerOrderHistory.js
+++ application/frontend/src/pages/Owner/OwnerOrderHistory.js
@@ -3,22 +3,26 @@ Summary of OwnerOrderHistory.js:
  - Renders on '/owner/orders'
  - to load when clicked on Orders on the Sidebar for Owner's login
 */
-import React, { useEffect, useState } from "react";
-import "../../assets/css/ownerlayout.css";
-import "../../assets/css/index.css";
-import axios from "axios";
+import React, { useEffect, useState } from 'react';
+import '../../assets/css/ownerlayout.css';
+import '../../assets/css/index.css';
+import axios from 'axios';
+import { useSelector } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 
 const OwnerOrderHistory = () => {
+  // redux global variable
+  const appUser = useSelector((state) => state.appUserReducer.appUser);
   const [orderItems, setOrderItems] = useState([]);
   const [orderContent, setOrderContent] = useState([]);
-  const [itemID, setItemID] = useState("");
+  const [itemID, setItemID] = useState('');
   const [orderStatus, setOrderStatus] = useState('');
   const [loadData, setLoadData] = useState(false);
 
   const handleOrderStatusChange = (event) => {
     event.preventDefault();
     axios
-      .post("http://localhost:3001/api/order/order-completed", null, {
+      .post('http://localhost:3001/api/order/order-completed', null, {
         params: { orderID: itemID },
       })
       .then((res) => {
@@ -28,129 +32,143 @@ const OwnerOrderHistory = () => {
   };
 
   useEffect(() => {
-    axios
-      .get("http://localhost:3001/api/order/user-orders", {
-        params: { restaurantName: "Taco Shell" },
-      })
-      .then((res) => {
-        setOrderItems(res.data);
-        setLoadData(false);
-      });
+    if (appUser.type === 'owner') {
+      axios
+        .get('http://localhost:3001/api/order/user-orders', {
+          params: { restaurantName: 'Taco Shell' },
+        })
+        .then((res) => {
+          setOrderItems(res.data);
+          setLoadData(false);
+        });
+    }
   }, [loadData]);
 
   return (
-    <div className="container-fluid">
-      <br />
-      <h3 className="owner-heading text-center"> Orders</h3>
+    <>
+      {appUser.type === 'owner' ? (
+        <div className="container-fluid">
+          <br />
+          <h3 className="owner-heading text-center"> Orders</h3>
 
-      {/* Orders Table */}
-      <div className="table-responsive-sm order-table">
-        <table class="table table-striped ">
-          <thead>
-            <tr class="table-secondary" className="order-list-title">
-              <th scope="col">Order #</th>
-              <th scope="col">Items</th>
-              <th scope="col">Driver</th>
-              <th scope="col">Customer</th>
-              <th scope="col">Price</th>
-              <th scope="col">Order Status</th>
-            </tr>
-          </thead>
+          {/* Orders Table */}
+          <div className="table-responsive-sm order-table">
+            <table class="table table-striped ">
+              <thead>
+                <tr class="table-secondary" className="order-list-title">
+                  <th scope="col">Order #</th>
+                  <th scope="col">Items</th>
+                  <th scope="col">Driver</th>
+                  <th scope="col">Customer</th>
+                  <th scope="col">Price</th>
+                  <th scope="col">Order Status</th>
+                </tr>
+              </thead>
 
-          <tbody>
-            {orderItems.length > 0 ? (
-              <>
-                {orderItems.map((item, index) => (
-                  <tr key={index}>
-                    <td>{item.ID}</td>
-                    <td>
-                      <button
-                        type="button"
-                        class="btn btn-outline-dark view-btn"
-                        data-toggle="modal"
-                        data-target="#viewModal"
-                        onClick={(e) => {
-                          setOrderContent(item.Order_Contents);
-                        }}
-                      >
-                        View
-                      </button>
-                    </td>
-                    <td>Marcus S.</td>
-                    <td>{item.Customer_Name}</td>
-                    <td>${item.Total}</td>
-                    <td>
-                      <select
-                        class="form-select order-status"
-                        onClick={(e) => {
-                          setItemID(item.ID);
-                          handleOrderStatusChange(); 
-                        }}
-                      >
-                        {item.Completed < 1 ? (
-                          <>
-                            <option value="progress" selected>
-                              In Progress
-                            </option>
-                            <option value="complete">Completed</option>
-                          </>
-                        ) : (
-                          <option value="complete" selected>
-                            Completed
-                          </option>
-                        )}
-                      </select>
-                    </td>
-                  </tr>
-                ))}
-              </>
-            ) : (
-              <> 
-              <label>Waiting to recieve orders...</label>
-              </>
-            )}
-          </tbody>
-        </table>
-      </div>
-      {/* View Modal */}
-      <div
-        class="modal fade"
-        id="viewModal"
-        data-backdrop="static"
-        tabindex="-1"
-        role="dialog"
-        aria-labelledby="viewModalLabel"
-        aria-hidden="true"
-      >
-        <div class="modal-dialog " role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="viewModalLabel">
-                Ordered Items
-              </h5>
-              <button
-                type="button"
-                class="close"
-                data-dismiss="modal"
-                aria-label="Close"
-              >
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div class="modal-body">
-              <ul class="list-group list-group-flush">
-                <li class="list-group-item">{orderContent}</li>
-              </ul>
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn modal-confirm-btn">
-                Confirm
-              </button>
+              <tbody>
+                {orderItems.length > 0 ? (
+                  <>
+                    {orderItems.map((item, index) => (
+                      <tr key={index}>
+                        <td>{item.ID}</td>
+                        <td>
+                          <button
+                            type="button"
+                            class="btn btn-outline-dark view-btn"
+                            data-toggle="modal"
+                            data-target="#viewModal"
+                            onClick={(e) => {
+                              setOrderContent(item.Order_Contents);
+                            }}
+                          >
+                            View
+                          </button>
+                        </td>
+                        <td>Marcus S.</td>
+                        <td>{item.Customer_Name}</td>
+                        <td>${item.Total}</td>
+                        <td>
+                          <select
+                            class="form-select order-status"
+                            onClick={(e) => {
+                              setItemID(item.ID);
+                              handleOrderStatusChange();
+                            }}
+                          >
+                            {item.Completed < 1 ? (
+                              <>
+                                <option value="progress" selected>
+                                  In Progress
+                                </option>
+                                <option value="complete">Completed</option>
+                              </>
+                            ) : (
+                              <option value="complete" selected>
+                                Completed
+                              </option>
+                            )}
+                          </select>
+                        </td>
+                      </tr>
+                    ))}
+                  </>
+                ) : (
+                  <>
+                    <label>Waiting to recieve orders...</label>
+                  </>
+                )}
+              </tbody>
+            </table>
+          </div>
+          {/* View Modal */}
+          <div
+            class="modal fade"
+            id="viewModal"
+            data-backdrop="static"
+            tabindex="-1"
+            role="dialog"
+            aria-labelledby="viewModalLabel"
+            aria-hidden="true"
+          >
+            <div class="modal-dialog " role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="viewModalLabel">
+                    Ordered Items
+                  </h5>
+                  <button
+                    type="button"
+                    class="close"
+                    data-dismiss="modal"
+                    aria-label="Close"
+                  >
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                  <ul class="list-group list-group-flush">
+                    <li class="list-group-item">{orderContent}</li>
+                  </ul>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn modal-confirm-btn">
+                    Confirm
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
+      ) : appUser.type === 'guest' ||
+        appUser.type === undefined ||
+        appUser.type === 'customer' ? (
+        <Redirect to="/" />
+      ) : appUser.type === 'driver' ? (
+        <Redirect to="/driver/current-orders" />
+      ) : (
+        <> </>
+      )}
+    </>
   );
 };
 

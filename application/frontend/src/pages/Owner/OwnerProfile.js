f9f6cfd76292345533f2791837468b27a16cc088
German Perez <germanp123@gmail.com>
2021-05-18 00:14:01 -0700

    owner profile styling

--- application/frontend/src/pages/Owner/OwnerProfile.js
+++ application/frontend/src/pages/Owner/OwnerProfile.js
@@ -1,12 +1,11 @@
 import React from "react";
-import restaurantcartoon from "../../assets/img/restaurant_ex.jpeg";
+//import restaurantcartoon from "../../assets/img/restaurant_ex.jpeg";
 import axios from "axios";
 import "../../assets/css/index.css";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 
 const OwnerProfile = () => {
-
   const [ownerInfo, setOwnerInfo] = useState([]);
 
   //restaurants from backend
@@ -16,7 +15,7 @@ const OwnerProfile = () => {
   const currentRestaurant = restaurantsList.filter(
     (restaurant) => restaurant.Name.trim() === "Taco Shell"
   );
-  
+
   const [loadData, setLoadData] = useState(false);
 
   //Renders owner info from DB
@@ -42,23 +41,11 @@ const OwnerProfile = () => {
           </div>
           <div className="editprofile" key={index}>
             {/* content header */}
-            <div className="border-bottom my-3 text-center">
-              <div className="row text-left">
-                <div class="col">
-                  <label for="name">
-                    {" "}
-                    <h5>
-                      <strong>{item.Name}</strong>
-                    </h5>
-                  </label>
-                </div>
-                <div class="col text-right">
-                  <label for="name">
-                    {" "}
-                    {item.Price_Level} • {item.Cuisine}, {item.Tags}
-                  </label>
-                </div>
-              </div>
+            <div className="border-bottom text-left">
+              {" "}
+              <h3>
+                <strong>{item.Name}</strong>
+              </h3>
             </div>
 
             {/* content body */}
@@ -66,55 +53,72 @@ const OwnerProfile = () => {
               <div class="profile-content" key={i}>
                 <div class="row">
                   <div class="col">
-                    <div class="row">
-                      <div class="col">
-                        <label className="form-descrip">
-                          <strong>Restaurant Owner</strong>
-                        </label>
-                      </div>
-
-                      <div class="col">
-                        <label for="name"> {ownerItem.Name}</label>
-                      </div>
-                    </div>
-                    <div class="row">
-                      <div class="col">
-                        <label className="form-descrip">
-                          <strong>Contact</strong>
-                        </label>
-                      </div>
-                      <div class="col">
-                        <label for="phone">{ownerItem.Phone}</label>
-                      </div>
-                    </div>
-                    <div class="row">
-                      <div class="col">
-                        <label className="form-descrip">
-                          <strong>Email</strong>
-                        </label>
-                      </div>
-                      <div class="col">
-                        <label for="email">{ownerItem.Email}</label>
-                      </div>
-                    </div>
-                    <div class="row">
-                      <div class="col">
-                        <label className="form-descrip">
-                          <strong>Restaurant Address</strong>
-                        </label>
-                      </div>
-                      <div class="col">
-                        <label for="address">{item.Address}</label>
-                      </div>
-                    </div>
+                    <label className="form-descrip">
+                      <strong>Restaurant Owner</strong>
+                    </label>
                   </div>
-                  <div class="col-6">
-                    <img
-                      src={restaurantcartoon}
-                      class="rounded img-thumbnail restImage"
-                      alt="Restaurant Img"
-                      width="300"
-                    />
+                  <div class="col">
+                    <label for="name"> {ownerItem.Name}</label>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col">
+                    <label className="form-descrip">
+                      <strong>Contact</strong>
+                    </label>
+                  </div>
+                  <div class="col">
+                    <label for="phone">{ownerItem.Phone}</label>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col">
+                    <label className="form-descrip">
+                      <strong>Email</strong>
+                    </label>
+                  </div>
+                  <div class="col">
+                    <label for="email">{ownerItem.Email}</label>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col">
+                    <label className="form-descrip">
+                      <strong>Restaurant Address</strong>
+                    </label>
+                  </div>
+                  <div class="col">
+                    <label for="address">{item.Address}</label>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col">
+                    <label className="form-descrip">
+                      <strong>Cuisine</strong>
+                    </label>
+                  </div>
+                  <div class="col">
+                    <label for="address">{item.Cuisine}</label>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col">
+                    <label className="form-descrip">
+                      <strong>Restaurant Tags</strong>
+                    </label>
+                  </div>
+                  <div class="col">
+                    <label for="address">{item.Tags}</label>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col">
+                    <label className="form-descrip">
+                      <strong>Price Level</strong>
+                    </label>
+                  </div>
+                  <div class="col">
+                    <label for="address">{item.Price_Level}</label>
                   </div>
                 </div>
               </div>
